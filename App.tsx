39507dd177d910fb3e2c3500cc2184fdac8bf3ed
Jose Carlos <josecarlos.soliscano@gmail.com>
2023-03-22 01:30:56 -0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  StyleSheet,
+  Image,
+  View,
+} from 'react-native';
+import Header from './components/Header';
+import { assets } from './react-native.config';
+import Formulario from './components/Formulario';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Image
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png') }
+      />
+
+      <View style={styles.contenido}>
+          <Formulario />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%'
+  },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
+});
+
+export default App;
